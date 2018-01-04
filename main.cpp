4d3b5e6e6a28470e6e16c86462773680cea30c0a
Cardstell <d>
2018-01-05 01:02:48 +0500

    first

---
+++ main.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <png.h>
+
+using namespace std;
+using namespace glm;
+
+const int width = 1920;
+const int height = 1080;
+const bool fullscreen = true;
+
+unsigned char rgb_data[width*height*3], argb_data[width*height*4];
+GLFWwindow *window;
+
+int init() {
+    if (!glfwInit()) {
+        cout<<"Error GLFW init"<<endl;
+        return -1;
+    }
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    window = glfwCreateWindow(width, height, "GLSL test", (fullscreen)?glfwGetPrimaryMonitor():NULL, NULL);
+    if (window == NULL) {
+        cout<<"Error opening window"<<endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK) {
+        cout<<"Error GLEW init"<<endl;
+        return -1;
+    }
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    //glEnable(GL_LIGHTING);
+    //glShadeModel(GL_SMOOTH);
+    //glEnable(GL_POLYGON_SMOOTH);
+    return 0;
+}
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path){
+    GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+    GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+    std::string VertexShaderCode;
+    std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+    if(VertexShaderStream.is_open()){
+        std::string Line = "";
+        while(getline(VertexShaderStream, Line))
+            VertexShaderCode += "\n" + Line;
+        VertexShaderStream.close();
+    }
+    else{
+        printf("Impossible to open %s\n", vertex_file_path);
+        getchar();
+        return 0;
+    }
+
+    std::string FragmentShaderCode;
+    std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+    if(FragmentShaderStream.is_open()){
+        std::string Line = "";
+        while(getline(FragmentShaderStream, Line))
+            FragmentShaderCode += "\n" + Line;
+        FragmentShaderStream.close();
+    }
+    GLint Result = GL_FALSE;
+    int InfoLogLength;
+
+    printf("Compiling shader : %s\n", vertex_file_path);
+    char const * VertexSourcePointer = VertexShaderCode.c_str();
+    glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
+    glCompileShader(VertexShaderID);
+    glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if ( InfoLogLength > 0 ){
+        std::vector<char> VertexShaderErrorMessage(InfoLogLength+1);
+        glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+        printf("%s\n", &VertexShaderErrorMessage[0]);
+    }
+
+    printf("Compiling shader : %s\n", fragment_file_path);
+    char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+    glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
+    glCompileShader(FragmentShaderID);
+    glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if ( InfoLogLength > 0 ){
+        std::vector<char> FragmentShaderErrorMessage(InfoLogLength+1);
+        glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+        printf("%s\n", &FragmentShaderErrorMessage[0]);
+    }
+
+    GLuint ProgramID = glCreateProgram();
+    glAttachShader(ProgramID, VertexShaderID);
+    glAttachShader(ProgramID, FragmentShaderID);
+    glLinkProgram(ProgramID);
+    glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+    glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if ( InfoLogLength > 0 ){
+        std::vector<char> ProgramErrorMessage(InfoLogLength+1);
+        glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+        printf("%s\n", &ProgramErrorMessage[0]);
+    }
+    glDetachShader(ProgramID, VertexShaderID);
+    glDetachShader(ProgramID, FragmentShaderID);
+    glDeleteShader(VertexShaderID);
+    glDeleteShader(FragmentShaderID);
+    return ProgramID;
+}
+
+void saveImage(string filename, unsigned char *data) {
+    FILE *fp = fopen(filename.c_str(), "wb");
+    if (!fp) {
+        cout<<"File save error"<<endl;
+        return;
+    }
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0);
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    png_init_io(png_ptr, fp);
+
+    png_set_IHDR(png_ptr, info_ptr, width, height,
+        8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+        PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+    png_write_info(png_ptr, info_ptr);
+    unsigned char *rows[height];
+    for (int i = 0;i<height;++i)
+        rows[i] = data + (i*width*3);
+    png_set_rows(png_ptr, info_ptr, rows);
+    png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, nullptr);
+    png_write_end(png_ptr, info_ptr);
+    png_destroy_write_struct(&png_ptr, nullptr);
+    fclose(fp);
+}
+
+void render() {
+    GLuint programID = LoadShaders("vertshader.vsh", "fragshader.fsh");
+    GLuint timeID = glGetUniformLocation(programID, "time");
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+    GLfloat vertex_buffer_data[] = {
+        -1.0f, -1.0f, -3.0f,
+        1.0f, -1.0f, -3.0f,
+        -1.0f, 1.0f, -3.0f,
+        1.0f, -1.0f, 1.2f,
+        -1.0f, 1.0f, 1.2f,
+        1.0f, 1.0f, -1.0f
+    };
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_DYNAMIC_DRAW);
+    float time = glfwGetTime();
+    int frames = 0;
+    bool screenshot = true;
+    cout<<endl;
+    float lastPressKey = glfwGetTime();
+    do {
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+        glUseProgram(programID);
+        glUniform1f(timeID, glfwGetTime());
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDisableVertexAttribArray(0);
+        frames++;
+        if (glfwGetTime() - time >= 1.0f) {
+            cout<<char(27)<<"[9D"<<char(27)<<
+                  "[1AFPS: "<<frames<<endl;
+            time += 1.0f;
+            frames = 0;
+        }
+        if (screenshot) {
+            glReadPixels(0,0,width,height,GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, argb_data);
+            for (int y = 0;y<height;++y) {
+                for (int x = 0;x<width;++x) {
+                    int i1 = (y*width+x)*3;
+                    int i2 = ((height-y-1)*width+x)*4;
+                    rgb_data[i1] = argb_data[i2+3];
+                    rgb_data[i1+1] = argb_data[i2+2];
+                    rgb_data[i1+2] = argb_data[i2+1];
+                }
+            }
+            saveImage("render.png", rgb_data);
+            screenshot = false;
+        }
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        if (glfwGetKey(window, GLFW_KEY_F2) == GLFW_PRESS && (glfwGetTime()-lastPressKey) > 0.3) {
+            screenshot = true;
+            lastPressKey = glfwGetTime();
+        }
+    }
+    while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window)==0);
+    glDeleteProgram(programID);
+    glfwTerminate();
+}
+
+int main()
+{
+    init();
+    render();
+    return 0;
+}
